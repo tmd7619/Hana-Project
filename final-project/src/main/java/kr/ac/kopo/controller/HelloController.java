6424c915a669415006063a3e95aaffcbef722fcc
tmd7619 <tmd814@naver.com>
2021-08-26 21:01:41 +0900

    new project

---
+++ final-project/src/main/java/kr/ac/kopo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package kr.ac.kopo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String helloJava() {
+		
+		return "/index";
+	}
+	
+	
+
+}
